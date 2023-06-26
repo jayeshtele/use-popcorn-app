99f1db49823669b3f3742931c11cac7f416b55d7
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-06-26 12:25:43 +0530

    Update StarRating.js

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,14 +1,30 @@
 import React from "react";
 
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const startContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
 export default function StarRating() {
   return (
-    <div>
-      <div>
+    <div style={containerStyle}>
+      <div style={startContainerStyle}>
         {Array.from({ length: 5 }, (_, i) => (
           <span>S{i + 1}</span>
         ))}
       </div>
-      <p>10</p>
+      <p style={textStyle}>10</p>
     </div>
   );
 }
